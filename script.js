f43693c8e40b8a163f97b646104da3d7304e8648
viktorwebjs <viktor.webjs@gmail.com>
2022-04-09 16:18:14 +0300

    finish

---
+++ script.js
@@ -0,0 +1,103 @@
+const BUSINESS_ENTITY_STATUSES = [
+  {
+    title: 'Not Validated',
+    value: 'not_validated',
+  },
+  {
+    title: 'Valid',
+    value: 'validated',
+  },
+  {
+    title: 'Validation Failed KYC',
+    value: 'validation_failed_kyc',
+  },
+  {
+    title: 'Validation Failed OFAC',
+    value: 'validation_failed_ofac',
+  },
+  {
+    title: 'Validation Failed Credit',
+    value: 'validation_failed_credit',
+  },
+  {
+    title: 'Validation Failed KYC Credit',
+    value: 'validation_failed_kyc_credit',
+  },
+];
+
+const PAYMENT_PLANS = [
+  {
+    id: 'gdh5h5151515h2t',
+    planName: 'Standart',
+  },
+  {
+    id: 'sth31srth5h11trsh',
+    planName: 'Low',
+  },
+  {
+    id: '5f515v1515f5',
+    planName: 'Max',
+  },
+];
+
+const businessEntities = [
+  {
+    city: 'Edmonton',
+    createdAt: '2022-03-28T13:41:06Z',
+    id: '51se5eg15ser153515es3rg',
+    merchantId: '',
+    name: 'Alex Inc',
+    planId: 'gdh5h5151515h2t',
+    state: 'US-AK',
+    status: 'not_validated',
+  },
+  {
+    city: 'Camptown',
+    createdAt: '2022-03-28T13:41:06Z',
+    id: 'srt3b881srt618618srt618t',
+    merchantId: '',
+    name: 'Alex Inc 2',
+    planId: 'gdh5h5151515h2t',
+    state: 'US-AK',
+    status: 'validated',
+  },
+  {
+    city: 'Chicago',
+    createdAt: '2022-03-28T13:41:06Z',
+    id: 'srth351srth15trh13',
+    merchantId: '',
+    name: 'Mego Inc',
+    planId: '5f515v1515f5',
+    state: 'US-AK',
+    status: 'validation_failed_ofac',
+  },
+  {
+    city: 'California',
+    createdAt: '2022-03-28T13:41:06Z',
+    id: 'srth7htsr1trsh424212strh',
+    merchantId: '',
+    name: 'Nick Inc',
+    planId: '5f515v1515f5',
+    state: 'US-AK',
+    status: 'validated',
+  },
+];
+
+const statusesMap = new Map();
+const paymentMap = new Map();
+let newUsers = [];
+
+BUSINESS_ENTITY_STATUSES.forEach(({ value, title }) =>
+  statusesMap.set(value, title)
+);
+PAYMENT_PLANS.forEach(({ id, planName }) => paymentMap.set(id, planName));
+newUsers = businessEntities.map(({ state, planId, status, name, id }) => {
+  return {
+    location: state,
+    status: statusesMap.get(status),
+    planName: paymentMap.get(planId),
+    name,
+    id,
+  };
+});
+console.log('newUsers :>> ', newUsers);
